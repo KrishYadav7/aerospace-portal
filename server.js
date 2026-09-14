23627c00a72257d269816bc92f56169f5feade53
VPS Deploy <deploy@aerogyan.tech>
2026-09-14 07:42:38 +0000

    Fix: serve static files from Express root

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(compression());
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static(__dirname));
 
 const apiLimiter = rateLimit({
   windowMs: 60 * 1000, max: 300,
